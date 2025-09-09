3e836e18f07ee16aebe4b15e7f2eae2a97832aa1
Jurako <jurako91@gmail.com>
2025-09-09 21:55:44 +0300

    remove test button

--- app/src/App.vue
+++ app/src/App.vue
@@ -1,9 +1,5 @@
 <template>
   <HeaderView />
-  <button
-    style="border: 1px solid black"
-    @click="testApi"
-  >Test button</button>
   <main class="mt-16 flex flex-wrap items-center justify-center gap-8 p-5 sm:mt-0">
     <RouterView />
   </main>
@@ -14,17 +10,6 @@ import { axiosBackend } from './config/axios';
 import HeaderView from './views/HeaderView.vue'
 
 export default {
-  data() {
-    return {
-      apiData: []
-    }
-  },
-  methods: {
-    testApi() {
-      axiosBackend.get('/')
-        .then(data => console.log(data))
-    }
-  },
   components: {
     HeaderView
   },
