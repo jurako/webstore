a1330b64c3153343dfa7fbccbb1cea898977c0e8
Jurako <jurako91@gmail.com>
2026-01-01 18:57:23 +0200

    fix bugs with storage values

--- app/src/stores/user.js
+++ app/src/stores/user.js
@@ -1,16 +1,21 @@
 import axios from '@/config/axios'
 import { defineStore } from 'pinia'
 
+let user = localStorage.getItem('user');
+user = (user !== 'undefined' && user !== 'null') ? JSON.parse(user) : {};
+
 const persistedData = {
-  user: JSON.parse(localStorage.getItem('user')),
-  isAuth: localStorage.getItem('isAuth')
+  user,
+  token: localStorage.getItem('token'),
+  isAuthenticated: localStorage.getItem('isAuthenticated') === 'true' ? true : false,
+  isVerified: localStorage.getItem('isVerified') === 'true' ? true : false
 }
 
 export const useUserStore = defineStore('user', {
   state: () => ({
     user: persistedData.user ?? {},
     token: persistedData.token ?? null,
-    isAuthenticated: persistedData.isAuth ?? false,
+    isAuthenticated: persistedData.isAuthenticated ?? false,
     isVerified: persistedData.isVerified ?? false
   }),
   getters: {
@@ -24,7 +29,7 @@ export const useUserStore = defineStore('user', {
         this.isAuthenticated = false
         this.isVerified = false
 
-        localStorage.removeItem('isAuth')
+        localStorage.removeItem('isAuthenticated')
         localStorage.removeItem('isVerified')
         localStorage.removeItem('user')
         localStorage.removeItem('token')
@@ -36,12 +41,12 @@ export const useUserStore = defineStore('user', {
       //pinia store
       this.user = data.user
       this.token = data.token
-      this.isVerified = data.user?.email_verified_at
+      this.isVerified = Boolean(data.user?.email_verified_at)
       this.isAuthenticated = true
 
       //local storage
-      localStorage.setItem('isAuth', true)
-      localStorage.setItem('isVerified', data.user?.email_verified_at)
+      localStorage.setItem('isAuthenticated', true)
+      localStorage.setItem('isVerified', Boolean(data.user?.email_verified_at))
       localStorage.setItem('user', JSON.stringify(data.user))
       localStorage.setItem('token', data.token)
     }
