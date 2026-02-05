d8c5c5cff72d4326afd24a582e35d6496d6ae8f6
Jurako <jurako91@gmail.com>
2026-02-05 21:33:52 +0200

    add home page

--- admin/src/router/index.js
+++ admin/src/router/index.js
@@ -1,15 +1,31 @@
 import { createRouter, createWebHistory } from 'vue-router'
+import useUserStore from '@/stores/user'
 import LoginView from '@/views/LoginView.vue';
+import HomeView from '@/views/HomeView.vue';
 
 const router = createRouter({
     history: createWebHistory(import.meta.env.BASE_URL),
     routes: [
         {
             path: '/',
+            name: 'home',
+            component: HomeView,
+            meta: { requiresAuth: true }
+        },
+        {
+            path: '/login',
             name: 'login',
             component: LoginView
         }
     ]
+});
+
+router.beforeEach((to, from) => {
+    const user = useUserStore();
+
+    if(to.meta.requiresAuth && !user.isAuth) {
+        return { name: 'login' }
+    }
 })
 
 export default router;
